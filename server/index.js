79f18723f6d4e7de1e286dde9b702e4e77da278c
zhalok <zhalok>
2022-08-25 22:35:51 +0600

    file uploading trial done

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ const recording_route = require("./routes/recording-route");
 const meeting_route = require("./routes/meeting-route");
 dotenv.config();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow_Headers", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  next();
+});
 app.use("/recordings", recording_route);
 app.use("/class", meeting_route);
 
